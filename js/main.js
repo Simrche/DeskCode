ee74ec6710edd8b1f0467dfcc090b9fe781bc782
Théodore <theodore.cadet@edu.devinci.fr>
2020-11-10 18:58:48 +0100

    :hammer: script waiting for popUp

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-function popUp(params) {
-    
+// POP UP -------------------------------------------------------------------------
+$(".that").hide();
+
+$('.this').click(function(){
+    $(".that").show();
+});
+
+
+// 
+document.getElementById(".pseudo").value=$pseudo;
+document.getElementById(".email").value=$email;
+document.getElementById(".mdp").value=$mdp;
+document.getElementById(".mdpVerif").value=$mdpVerif;
+
+// pour pouvoir afficher/activer le bouton
+if ($pseudo.lenght>0 && $email.lenght>0 && $mdp.lenght>0 && $mdpVerif.lenght>0) {
+    $(".that").show(); 
 }
+
