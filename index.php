690e46f1467a2615d7e12390bc40f892ff6eb699
Cameron Lormont <cameron.celerier_dit_lormont@edu.devinci.fr>
2020-11-17 18:00:25 +0100

    cod

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
                 <a href="csgo.php">CS:GO</a>
                 <a href="fifa.php">FIFA 21</a>
                 <a href="#">Smash Bros Ultimate</a>
-                <a href="#">Call of Duty: Warzone</a>
+                <a href="cod.php">Call of Duty: Warzone</a>
             </div>
         </section>
 
