fb226b097cdff960145c52b4bca8a0032d39dd4d
Théodore <theodore.cadet@edu.devinci.fr>
2021-04-21 11:43:04 +0200

    poo bracketform

--- Public/BracketForm.php
+++ Public/BracketForm.php
@@ -6,7 +6,7 @@ $bdd = new PDO("mysql:host=localhost;dbname=deskcode;charset=utf8", $indivRoots,
 $ajoutUsers = $bdd->prepare("INSERT INTO users(users_pseudo, users_mdp, users_email) VALUES(?, ?, ?);");
 $connexion =  $bdd->prepare('SELECT users_mdp FROM users WHERE users_pseudo=:pseudo');
 
-
+use App\BracketForm;
 use App\Site;
 use App\SignInC;
 
@@ -38,33 +38,12 @@ SignInC::deconnexionU();
     Site::headerSite();
     ?>
         <div id="BrackForm">
+            <!-- <h1>Créer un nouveau Bracket</h1> -->
             <form action="">
                 <div>
-                    <label for="">Date: </label><input type="text">
-
-                    <label for="">Lieu: </label><input type="text">
-
-                    <label for="">Nombre d'équipes: </label><input type="text">
-
-                    <label for="">Loser Bracket: </label><div><input type="radio" name="LoserRad">OUI<input type="radio" name="LoserRad">NON</div>
-
-                    <label for="">Match: </label>
-                    <select name="pets" id="pet-select">
-                        <option value="">--Please choose an option--</option>
-                        <option value="bo1">BO1</option>
-                        <option value="bo2">BO3</option>
-                        <option value="bo3">BO5</option>
-                    </select>
-
-                    <label for="">Match finale: </label><div><input type="radio" name="FinalRad">BO1<input type="radio" name="FinalRad">BO3<input type="radio" name="FinalRad">BO5</div>
-
-                    <label for="">1er prix: </label><input type="text">
-
-                    <label for="">Second Prix: </label><input type="text">
-
-                    <label for="">Troisième prix: </label><input type="text">
-
-                    <label for="">Description: </label><textarea></textarea>
+                    <?php
+                    BracketForm::labels() 
+                    ?>
                 </div>
             </form>
         </div>
